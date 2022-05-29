b9cd81527217fc71a49d30a7da749ed6627b993f
ansquf0715 <48300642+ansquf0715@users.noreply.github.com>
2022-05-29 17:23:04 +0900

    Add files via upload

---
+++ 4-3.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstring>
+
+int findMaxValue(int a[], int len)
+{
+	int maxVal = a[0];
+	for (int i = 1; i < len; i++)
+		if (maxVal < a[i]) maxVal = a[i];
+	return maxVal;
+}
+
+char findMaxChar(char a[])
+{
+	char maxChar = a[0];
+	for (int i = 1; i < strlen(a); i++)
+		if (maxChar < a[i]) maxChar = a[i];
+	return maxChar;
+}
+
+int main()
+{
+	int arr[10] = { 3,24,82,12,34,7,53,17,26,51 };
+	char str[] = "game over!";
+	printf("max val = %d\n", findMaxValue(arr, 10));
+	printf("max char = %c\n", findMaxChar(str));
+}
