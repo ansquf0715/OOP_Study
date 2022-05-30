db5f0e2f3c41bcb4ebf98d469d0a8fc0cfe1bff8
ansquf0715 <48300642+ansquf0715@users.noreply.github.com>
2022-05-30 19:03:18 +0900

    Add files via upload

---
+++ 4-6.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+
+struct Complex {
+	double real;
+	double imag;
+};;
+
+void printComplex(Complex c, const char* msg = "복소수")
+{
+	printf("%s %4.2f + %4.2fi\n", msg, c.real, c.imag);
+}
+
+Complex readComplex(const char* msg = "복소수")
+{
+	Complex c;
+	printf(" %s ", msg);
+	scanf("%lf%lf", &c.real, &c.imag);
+	return c;
+}
+
+Complex addComplex(Complex a, Complex b)
+{
+	Complex c;
+	c.real = a.real + b.real;
+	c.imag = a.imag + b.imag;
+	return c;
+}
+
+void resetComplex(Complex a) { a.real = a.imag = 0.0; } //???
+
+int main()
+{
+	Complex a, b, c;
+	a = readComplex("a(input) = ");
+	b = readComplex("b(input) = ");
+	c = addComplex(a, b);
+
+	printComplex(a, "a = ");
+	printComplex(b, "b = ");
+	printComplex(c, "a+b = ");
+	resetComplex(c);
+	printComplex(c, "reset(c)=");
+}
